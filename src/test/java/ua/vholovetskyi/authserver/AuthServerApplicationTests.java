f33f094a403dc5416cf9ed41918c2d35bc8af788
Volodymyr Holovetskyi <vholovetskyi@gmail.com>
2024-06-15 14:30:14 +0200

    Start project

---
+++ src/test/java/ua/vholovetskyi/authserver/AuthServerApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.vholovetskyi.authserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
